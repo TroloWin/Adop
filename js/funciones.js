34ee6676d59f2e7b52a119319a880298eff494a5
TroloWin <trolowin9@gmail.com>
2026-02-28 10:01:35 -0600

    1.0
    Base

--- js/funciones.js
+++
@@ -1,7 +0,0 @@
-// funciones javascript
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('pagina cargada correctamente');
-    
-    // aqui iran las funciones futuras
-});
